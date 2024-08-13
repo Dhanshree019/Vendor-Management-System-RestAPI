2f694f09aa02f4e3464e876dd90f12ffea0dffd9
Dhanshree Patangrao <dhanshree.patangrao@gmail.com>
2024-08-13 12:44:18 +0530

    env added

--- vendor_management_system/settings.py
+++ vendor_management_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +85,9 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
